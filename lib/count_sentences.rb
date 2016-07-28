cb9c3bcee56abec92e6d693cd67076c8e38f5b96
Jacob Edgerly-Dowd <dowdje89@gmail.com>
2016-07-28 10:25:31 +0000

    Count_sentences lab completed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    sentence_array = self.split("")
+    for i in 0...sentence_array.length
+      if sentence_array[i] == sentence_array[i + 1] || sentence_array[i] == sentence_array[i - 1]
+        sentence_array.delete_at(i)
+      end
+    end
+    sentence_array.count(".") + sentence_array.count("?") + sentence_array.count("!")
   end
 end
